50b78a9b7fde47a25521aa65d2a0c4a87a371d56
alprince <132316008+princeprog@users.noreply.github.com>
2025-09-11 00:17:40 +0800

    Update SecurityConfiguration.java

--- src/main/java/com/startupsphere/capstone/configs/SecurityConfiguration.java
+++ src/main/java/com/startupsphere/capstone/configs/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
