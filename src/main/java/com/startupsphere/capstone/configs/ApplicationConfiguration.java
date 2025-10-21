84b3b0a99a6f2ea06713f50efdca0ada5e52e554
alprince <132316008+princeprog@users.noreply.github.com>
2025-10-21 21:40:16 +0800

    Update database connection settings
    Changed MySQL datasource URL and password in application.properties to connect to a remote Railway database. No functional code changes, only configuration update.

--- src/main/java/com/startupsphere/capstone/configs/ApplicationConfiguration.java
+++ src/main/java/com/startupsphere/capstone/configs/ApplicationConfiguration.java
@@ -39,7 +39,6 @@ public class ApplicationConfiguration {
     @Bean
     AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
 
